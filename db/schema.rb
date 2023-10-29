67c1c4b87ed81223377e783d6ed96b804517c3af
Ivar-7 <leank6071@gmail.com>
2023-10-29 18:17:08 +0300

    generate service scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_29_135930) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_29_151640) do
+  create_table "services", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "picture"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "username"
+    t.string "profile_picture"
+    t.integer "phone_number"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
