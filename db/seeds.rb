486225533d5982665342aa47f66cd802f7e40f39
Ivar-7 <leank6071@gmail.com>
2023-10-29 18:32:21 +0300

    seed data for services

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,21 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+# Seed services
+common_services = [
+    { name: "House Cleaning", description: "Professional house cleaning services.", picture: "" },
+    { name: "Lawn Care", description: "Lawn mowing and maintenance.", picture: "" },
+    { name: "Plumbing Repairs", description: "Fixing plumbing issues and leaks.", picture: "" },
+    { name: "Pet Sitting", description: "Professional pet sitting and dog walking.", picture: "" },
+    { name: "Electrical Repairs", description: "Electrical wiring and fixture repairs.", picture: "" },
+    { name: "Interior Painting", description: "Interior painting and color consultation.", picture: "" },
+    { name: "Furniture Assembly", description: "Assembly of furniture and IKEA items.", picture: "" },
+    { name: "Gardening Services", description: "Landscaping and garden maintenance.", picture: "" },
+    { name: "Computer Repair", description: "Computer and laptop troubleshooting and repair.", picture: "" },
+    { name: "Tutoring Services", description: "Educational tutoring for various subjects.", picture: "" }
+  ]
+  
+puts "Seeding services..."
+common_services.each do |service|
+    Service.create(service)
+end
+puts "Services seeded!"
+  
