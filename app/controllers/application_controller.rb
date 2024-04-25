6d634f5d2abaa315d23e3b0fa62222fa3311b1fe
Ivar-7 <leank6071@gmail.com>
2024-04-25 15:17:19 +0300

    redirect to taskers after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
 
+  def after_sign_in_path_for(resource)
+    taskers_path
+  end
+
   private
   def authenticate_user!
     redirect_to new_user_session_path unless user_signed_in?
